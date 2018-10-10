7cc81239d33203b218c2438dfe7925ed905cd719
1598692232 <1598692232@qq.com>
2018-10-10 11:15:23 +0800

    修改数据加载动画

---
+++ app/src/components/common/Loading/Loading.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class Loading extends PureComponent {
+
+	render() {
+		return (
+            <div className="loading-wrapper">
+            	<div className="loading-common"></div>
+            </div> 
+		);
+	}
+}
+
+
+export default Loading;
--- app/src/components/common/Loading/index.js
+++ app/src/components/common/Loading/index.js
@@ -1,23 +1,27 @@
 import React, { PureComponent } from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import LoadingIcon from './LoadingIcon';
+import Animate from 'rc-animate';
+import Dialog from 'rc-dialog';
+
+import LoadingIcon from './Loading';
 import './index.scss';
 
 class Loading extends PureComponent {
-
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
-		if (!this.props.visible) return null; 
 		return (
-			<div className="loading-container">
-				{/* <div className="loading-animate">
-				</div> */}
-				<LoadingIcon></LoadingIcon>
-			</div>
+			<Dialog visible closable={false}>
+				<Animate 
+					transitionName="fade"
+					transitionAppear
+				>
+				{this.props.visible ? 
+					<div className="loading-container" >
+						{/* <div className="loading-animate">
+						</div> */}
+						<LoadingIcon></LoadingIcon>
+					</div>: null}
+				</Animate>
+			</Dialog>
 		);
 	}
 }
